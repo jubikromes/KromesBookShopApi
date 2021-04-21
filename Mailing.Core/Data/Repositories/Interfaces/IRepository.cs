0fff19956c75513bbafb10e6f5dfc2fb8ff290dc
kome <kome.okah-avae@outlook.com>
2021-04-21 22:34:16 +0100

    latest commit

--- Mailing.Core/Data/Repositories/Interfaces/IRepository.cs
+++ Mailing.Core/Data/Repositories/Interfaces/IRepository.cs
@@ -21,59 +21,26 @@ namespace Mailing.Core.Repositories.Interfaces
         Task<TEntity> GetByIdAsync(Guid id);
 
         Task<int> Count(Expression<Func<TEntity, bool>> predicate);
-        /// <summary>
-        /// Get query for entity
-        /// </summary>
-        /// <param name="filter"></param>
-        /// <param name="orderBy"></param>
-        /// <returns></returns>
+        
         IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null);
 
-        /// <summary>
-        /// Get single entity by primary key
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+       
         TEntity GetById(object id);
 
-        /// <summary>
-        /// Get first or default entity by filter
-        /// </summary>
-        /// <param name="filter"></param>
-        /// <param name="includes"></param>
-        /// <returns></returns>
+       
         TEntity GetFirstOrDefault(
             Expression<Func<TEntity, bool>> filter = null,
             params Expression<Func<TEntity, object>>[] includes);
 
-        /// <summary>
-        /// Insert entity to db
-        /// </summary>
-        /// <param name="entity"></param>
+      
         void Insert(TEntity entity);
 
-        /// <summary>
-        /// Insert multiple entities to db
-        /// </summary>
-        /// <param name="entities"></param>
         void InsertRange(IEnumerable<TEntity> entities);
 
-        /// <summary>
-        /// Insert Or Update entity to db
-        /// </summary>
-        /// <param name="entity"></param>
-        //void InsertOrUpdate(TEntity entity);
-
-        /// <summary>
-        /// Update entity in db
-        /// </summary>
-        /// <param name="entity"></param>
+        
         void Update(TEntity entity);
 
-        /// <summary>
-        /// Delete entity from db by primary key
-        /// </summary>
-        /// <param name="id"></param>
+       
         void Delete(TEntity entity);
 
         void Remove(TEntity entityToDelete);
